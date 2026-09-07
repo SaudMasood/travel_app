0bce395d7f0aac4a455866afd53e2cc9ef32b717
Saud Masood <saudmasood974@gmail.com>
2026-09-07 21:14:34 +0500

    Update project

--- lib/features/info/block/info_bloc.dart
+++ lib/features/info/block/info_bloc.dart
@@ -120,7 +120,6 @@ class InfoBloc extends Bloc<InfoEvent, InfoState> {
           countries: countries,
         ),
       );
-//
       print('ALL INFO APIs COMPLETED');
     } catch (e) {
       print('INFO API ERROR: $e');
